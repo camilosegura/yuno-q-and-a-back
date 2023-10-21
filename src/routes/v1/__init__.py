6edfa885e30058990733dff6e564527922e02acd
camiloLizarazo <camilo.lizarazo@y.uno>
2023-10-21 17:52:05 -0500

    esta si fue

--- src/routes/v1/__init__.py
+++ src/routes/v1/__init__.py
@@ -3,4 +3,4 @@ from .example_routing import example_router
 
 api_v1 = APIRouter()
 
-api_v1.include_router(prefix="/example", router=example_router)
+api_v1.include_router(prefix="/assistant", router=example_router)
--- src/routes/v1/example_routing.py
+++ src/routes/v1/example_routing.py
@@ -1,13 +1,44 @@
 from controllers import example_controller
 from fastapi import APIRouter
 from handlers.errors import has_errors
+from typing import Union
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain.chat_models import ChatOpenAI
+import sys
+import os
 
+os.environ["OPENAI_API_KEY"] = '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'
 example_router = APIRouter()
+tags = ['example'] 
+diretorio_atual = os.path.dirname(__file__)
+caminho_relativo = os.path.join(diretorio_atual, "index.json")
+docsPath = os.path.join(diretorio_atual, "docs")
+index = GPTSimpleVectorIndex.load_from_disk(caminho_relativo)
 
-tags = ['example']
+def construct_index(directory_path):
+    max_input_size = 4096
+    num_outputs = 512
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
+
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.2, model_name="text-embedding-ada-002", max_tokens=num_outputs))
+    documents = SimpleDirectoryReader(directory_path).load_data()
+    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+    index.save_to_disk(caminho_relativo)
+    return "Training finished"
+
+def chatbot(input_text):
+    response = index.query(input_text, response_mode="compact")
+    return response.response
 
 
-@example_router.get('/get/{code}', tags=tags)
-@has_errors
-async def example_get(code: int):
-    return example_controller.example_get(code)
+@example_router.get("/gpt/training")
+def read_root():
+    return construct_index(docsPath)
+
+
+@example_router.get("/gpt")
+def read_item(q: Union[str, None] = None):
+    return chatbot(q) 
+
