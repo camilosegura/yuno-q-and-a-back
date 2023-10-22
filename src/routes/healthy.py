03966684798d753b3d4d6d6cf7dbad000fc290c8
camiloLizarazo <camilo.lizarazo@y.uno>
2023-10-21 19:22:35 -0500

    fix deploy

--- src/routes/healthy.py
+++ src/routes/healthy.py
@@ -1,46 +1,21 @@
 from fastapi import APIRouter
-from typing import Union
-from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-from langchain.chat_models import ChatOpenAI
-import sys
-import os
+from controllers import healthy_controller
 
-os.environ["OPENAI_API_KEY"] = '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'
 healthy_router = APIRouter()
+
 tags = ["healthy"]
-diretorio_atual = os.path.dirname(__file__)
-caminho_relativo = os.path.join(diretorio_atual, "index.json")
-docsPath = os.path.join(diretorio_atual, "docs")
-indexDocs = ""
-def construct_index(directory_path):
-    max_input_size = 4096
-    num_outputs = 512
-    max_chunk_overlap = 20
-    chunk_size_limit = 600
-
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.2, model_name="text-embedding-ada-002", max_tokens=num_outputs))
-    documents = SimpleDirectoryReader(directory_path).load_data()
-    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-    index.save_to_disk(caminho_relativo)
-    indexDocs = GPTSimpleVectorIndex.load_from_disk(caminho_relativo)
-    return "Training finished"
-
-def chatbot(input_text):
-    response = indexDocs.query(input_text, response_mode="compact")
-    return response.response
 
 
-@healthy_router.get('/')
-def root():
-    return "Hola healthy"
-
-@healthy_router.get('/training')
-def read_root():
-    return construct_index(docsPath)
+@healthy_router.get('/', tags=tags, include_in_schema=False)
+async def root():
+    return healthy_controller.root()
 
 
-@healthy_router.get('/healthy')
-def healthy(q: Union[str, None] = None):
-    return chatbot(q) 
+@healthy_router.get('/healthy', tags=tags, include_in_schema=False)
+async def healthy():
+    return healthy_controller.healthy()
 
+
+@healthy_router.get('/liveness', tags=tags, include_in_schema=False)
+async def liveness():
+    return healthy_controller.liveness()
