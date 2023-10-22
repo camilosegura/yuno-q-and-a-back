78dcf391c392033bcbe9143147a849c1e6d76b86
camiloLizarazo <camilo.lizarazo@y.uno>
2023-10-21 19:21:34 -0500

    fix deploy

--- src/routes/v1/example_routing.py
+++ src/routes/v1/example_routing.py
@@ -13,22 +13,24 @@ tags = ['example']
 diretorio_atual = os.path.dirname(__file__)
 caminho_relativo = os.path.join(diretorio_atual, "index.json")
 docsPath = os.path.join(diretorio_atual, "docs")
+indexDocs = ""
 
 def construct_index(directory_path):
+    global indexDocs
     max_input_size = 4096
     num_outputs = 512
     max_chunk_overlap = 20
     chunk_size_limit = 600
-
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
     llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.2, model_name="text-embedding-ada-002", max_tokens=num_outputs))
     documents = SimpleDirectoryReader(directory_path).load_data()
     index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
     index.save_to_disk(caminho_relativo)
+    indexDocs = GPTSimpleVectorIndex.load_from_disk(caminho_relativo)
     return "Training finished"
 
 def chatbot(input_text):
-    response = index.query(input_text, response_mode="compact")
+    response = indexDocs.query(input_text, response_mode="compact")
     return response.response
 
 
